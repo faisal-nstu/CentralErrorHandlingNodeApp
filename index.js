ffc3caf1c60f4e19afd6f11a7bd604b12ace11a2
Faisal Amin <faisal.nstu@ymail.com>
2020-07-14 20:13:18 +0600

    error endpoint

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
   return res.status(200).json({ message: 'Hello World' });
 });
 
+app.get('/error', (req, res) => {
+  throw new Error('Internal server error.')
+})
+
 app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
