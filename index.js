0cf24fcc217886c02ba274018552b47a636a8bc7
Faisal Amin <faisal.nstu@ymail.com>
2020-07-14 20:11:12 +0600

    initial express config

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Hello World' });
+});
+
+app.listen(PORT, () => console.log(`Server listening at port ${PORT}`));
